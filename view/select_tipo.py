d0bd48cda8dfbea431603bae02023734ce52f668
Mateus <mathiesen.geomais@gmail.com>
2019-11-21 11:29:17 -0200

    override open select tipo

--- view/select_tipo.py
+++ view/select_tipo.py
@@ -11,3 +11,12 @@ class SelectTipo(AbstractTela):
     def components(self):
         layout = [[sg.Button('Usuário', size=(10, 1)), sg.Button('Segurança', size=(10, 1))]]
         self.window = sg.Window('Selecionar Tipo').Layout(layout)
+
+    def open(self):
+        self.unhide()
+        button, values = self.window.Read()
+        self.hide()
+        if button == "Usuário":
+            return TipoPessoa.USUARIO
+        else:
+            return TipoPessoa.SEGURANCA
