6444bebf371e4be582c337dd5b16f19f4259e25b
Mateus <mateusthiesen@gmail.com>
2019-11-21 02:06:54 -0300

    add component with list

--- view/tela_cadastro.py
+++ view/tela_cadastro.py
@@ -1,6 +1,7 @@
 from view.abstract_tela import AbstractTela
 from getpass import getpass
 from model.tipo import TipoPessoa
+import PySimpleGUI as sg
 
 
 class TelaCadastro(AbstractTela):
@@ -8,19 +9,22 @@ class TelaCadastro(AbstractTela):
     def __init__(self):
         super().__init__()
 
-    def components(selfs):
-        pass
-
-    def lista_pessoas(self, pessoas: []):
-        print(pessoas)
-        print("Listando...")
-        print("Nome\t-\tTelefone\t-\tId")
+    def components(self, tipo=TipoPessoa.USUARIO, pessoas=None):
+        if pessoas is None:
+            pessoas = {}
+        lista = ["Nome - Telefone - ID"]
         for pessoa in pessoas:
-            print(pessoa.nome
-                  + "\t-\t"
-                  + str(pessoa.telefone)
-                  + "\t-\t"
-                  + (str(pessoa.codigo) if hasattr(pessoa, "codigo") else pessoa.matricula))
+            identificador = pessoa.codigo if hasattr(pessoa, "codigo") else pessoa.matricula
+            lista.append((pessoa.nome
+                          + " - "
+                          + str(pessoa.telefone)
+                          + " - "
+                          + str(identificador)))
+
+        layout = [[sg.Listbox(lista, size=(50, 6))],
+                  [sg.Button('Novo', size=(10, 1)), sg.Button('Editar', size=(10, 1)),
+                   sg.Button('Excluir', size=(10, 1)), sg.Button('Início', size=(10, 1))]]
+        self.window = sg.Window('Menu Cadastro ' + tipo.name).Layout(layout)
 
     def cadastro(self, tipo: TipoPessoa, novo=True):
         respostas = {}
