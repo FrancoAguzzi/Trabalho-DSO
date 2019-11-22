f806a4a8b89a03335ed0c00162f4f06be42213e7
Mateus <mathiesen.geomais@gmail.com>
2019-11-22 14:44:18 -0200

    remove unused functions

--- view/tela_cadastro.py
+++ view/tela_cadastro.py
@@ -25,28 +25,3 @@ class TelaCadastro(AbstractTela):
                   [sg.Button('Novo', size=(10, 1)), sg.Button('Editar', size=(10, 1)),
                    sg.Button('Excluir', size=(10, 1)), sg.Button('Início', size=(10, 1))]]
         self.window = sg.Window('Menu Cadastro ' + tipo.name).Layout(layout)
-
-    def cadastro(self, tipo: TipoPessoa, novo=True):
-        respostas = {}
-        if novo:
-            print("Novo Cadastro...")
-        else:
-            print("Atualizando Cadastro...")
-
-        if tipo == TipoPessoa.USUARIO:
-            respostas["matricula"] = input("Matrícula: ")
-        else:
-            respostas["codigo"] = input("Código: ")
-            respostas["senha_especial"] = getpass("Senha Especial: ")
-
-        respostas["nome"] = input("Nome: ")
-        respostas["telefone"] = input("Telefone: ")
-
-        return respostas
-
-    def excluir(self):
-        respostas = {"id": input("Digite o identificador da pessoa a ser excluida (código/matrícula): ")}
-        return respostas
-
-    def mostra_informacao(self, info):
-        print(info)
